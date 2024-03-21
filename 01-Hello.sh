bef77b6be1d829d43d5691fc4b857452c6954d55
Praveen Kothapally <praveenkothapally18@gmail.com>
2024-03-22 00:27:35 +0530

    message

--- 01-Hello.sh
+++ 01-Hello.sh
@@ -7,6 +7,5 @@ Person2=Shekar
 echo "$Person1: Hi This is Praveen"
 echo "$Person2: Hi I am Shekar"
 echo "$Person1: How are you"
-echo "$Person2: I am good,Thank you for asking"
-echo "$person2: What about you" $n 
+echo "$Person2: I am good,Thank you for asking. How are you ?"
 echo "$Person1: I am doing fine, Thank you"
