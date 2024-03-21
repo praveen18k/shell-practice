0d371c454a6b7353e33d79443c702d3d8eb80e01
Praveen Kothapally <praveenkothapally18@gmail.com>
2024-03-22 01:11:20 +0530

    Added new lines

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1: Hi This is Praveen"
+echo "$Person2: Hi I am Shekar"
+echo "$Person1: How are you"
+echo "$Person2: I am good,Thank you for asking. How are you ?"
+echo "$Person1: I am doing fine, Thank you"
