b78e2699f4d895fd209ae56905e7a83ea0728d0e
Praveen Kothapally <praveenkothapally18@gmail.com>
2024-03-22 00:21:02 +0530

    message

--- 01-Hello.sh
+++ 01-Hello.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
 echo "Hello this is my practice session"
-echo "Started with Shell Script"
 #we use variables that follows DRY - principles
+
+Person1=Praveen
+Person2=Shekar
+
+echo "$Person1: Hi This is Praveen"
+echo "$Person2: Hi I am Shekar"
+echo "$Person1: How are you"
+echo "$Person2: I am good,Thank you for asking"
+echo "$person2: What about you"
+echo "$Person1: I am doing fine, Thank you"
