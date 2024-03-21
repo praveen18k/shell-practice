ecc9829e7af08a1d5dd2575a0067956fd36d3864
Praveen Kothapally <praveenkothapally18@gmail.com>
2024-03-22 01:15:39 +0530

    Added new lines

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-Person1=$1
-Person2=$2
+Person1=$1 #this is the first value from command line
+Person2=$2 #thit is the seconf value from command line
 
 echo "$Person1: Hi This is Praveen"
 echo "$Person2: Hi I am Shekar"
 echo "$Person1: How are you"
 echo "$Person2: I am good,Thank you for asking. How are you ?"
 echo "$Person1: I am doing fine, Thank you"
+
+
+#Provide the values($1 and $2) from command line 
+#Example: $ sh-variables.sh Praveen Shekar
