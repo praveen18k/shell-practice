f83ac156c07feeb9307b8d88316d7db8e1a83911
Praveen Kothapally <praveenkothapally18@gmail.com>
2024-03-26 01:14:43 +0530

    Updated install git file

--- 08-installgit.sh
+++ 08-installgit.sh
@@ -6,8 +6,11 @@
 #Note - $? --> store the previous command status.
 #Note - $? --> 0 then previous command is success. (ex: echo $? = 0)
 #Note - $? --> not 0 then previous command is failuer. (ex: echo $? = 127 : exit status 1 to 127 anything is failuer)
+#Note - less lines of code, more work. reduce the no of lines of code. use functions.
 #Q)This is the imp interview @ what is exit status ?
 #A- By default shell script cannot understand the previous command is success or failure, it is our responsibility to check that is through the Exit status, if exit status is 0 we can consider it as sucess if exit status is 1 to 127 we can consider it as failure.
+
+#**Functions**: You will keep some code inside functions which is more often repeated, and call whenever you required.
 
 #Script goal is to install MySql
 
