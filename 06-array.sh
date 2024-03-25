e0e7eec59443ff4d4e48949e7ac74776481c00c4
Praveen Kothapally <praveenkothapally18@gmail.com>
2024-03-25 15:39:36 +0530

    Added new lines

--- 06-array.sh
+++ 06-array.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+#this is array, insted of single value it can hold multiple values.
+
+Persons=("Praveen", "Naveen", "Shekar")
+
+#inside array we always refer values with index and it starts from 0
+
+echo "First Person: ${Persons[0]}"
