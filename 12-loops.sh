269740207a77861466ada45f2d7baf3f1c0893d6
Praveen <praveenkothapally18@gmail.com>
2024-03-26 23:51:26 +0530

    added asignment,

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+
+
 #all args are in $@
 for i in $@
 do
     yum install $i -y
 done
+
+#improvements
+#implement log files
+#implement colors
+#implement validations
+#implement validations through functions
+#your script should check package is already installed or not, if already installed print yellow color
+#if installed just print package is already installed, it should not run install command
